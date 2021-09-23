1ff7dfc5b563df27d74e05d815239f45683eb2c3
j-malino <jessica.gvsu@gmail.com>
2021-09-23 18:36:34 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace EmguCVDemoJmalino
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.process = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemShapeMatching = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.process});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1868, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpen,
+            this.toolStripMenuItemExit});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItemFile.Text = "File";
+            // 
+            // toolStripMenuItemOpen
+            // 
+            this.toolStripMenuItemOpen.Name = "toolStripMenuItemOpen";
+            this.toolStripMenuItemOpen.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuItemOpen.Text = "Open";
+            this.toolStripMenuItemOpen.Click += new System.EventHandler(this.toolStripMenuItemOpen_Click);
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            // 
+            // process
+            // 
+            this.process.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemShapeMatching});
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(59, 20);
+            this.process.Text = "Process";
+            // 
+            // toolStripMenuItemShapeMatching
+            // 
+            this.toolStripMenuItemShapeMatching.Name = "toolStripMenuItemShapeMatching";
+            this.toolStripMenuItemShapeMatching.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemShapeMatching.Text = "Shape Matching";
+            this.toolStripMenuItemShapeMatching.Click += new System.EventHandler(this.toolStripMenuItemShapeMatching_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblmessage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1868, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.AutoSize = true;
+            this.lblmessage.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblmessage.Location = new System.Drawing.Point(12, 0);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(0, 19);
+            this.lblmessage.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 64);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1868, 860);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1868, 924);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Emgu CV 4.5.3";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblmessage;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem process;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemShapeMatching;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace EmguCVDemoJmalino
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Image<Bgr, byte>> IMGDict;
+        public Form1()
+        {
+            InitializeComponent();
+            IMGDict = new Dictionary<string, Image<Bgr, byte>>();
+        }
+
+        private void toolStripMenuItemOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Image Files (*.jpg;*.png;*.bmp;)|*.jpg;*.png;*.bmp;|All Files (*.*)|*.*;";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var img = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.Image = img.ToBitmap();
+                    if (IMGDict.ContainsKey("input"))
+                    {
+                        IMGDict.Remove("input");
+                    }
+                    IMGDict.Add("input",img);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void toolStripMenuItemShapeMatching_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // reads stop sign file from computer, default template to match
+                Image<Bgr, byte> imgTemplate = new Image<Bgr, byte>(@"C:\Users\jessa\OneDrive - Grand Valley State University\Documents\GVSU\Graduate Classes\CIS 641 Systems Analysis and Design\STOP-sign.jpg");
+                ApplyShapeMatching(imgTemplate);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ApplyShapeMatching(Image<Bgr, byte> imgTemplate, double threshold = 0.0001)
+        {
+            try
+            {
+                if(IMGDict["input"]==null)
+                {
+                    throw new Exception("Select an Image");
+                }
+
+
+                var img = IMGDict["input"].Clone();
+                var imgSource = img.Convert<Gray, byte>()
+                    .SmoothGaussian(3)
+                    .ThresholdBinaryInv(new Gray(240), new Gray(255));
+
+                var imgTarget = imgTemplate.Convert<Gray, byte>()
+                    .SmoothGaussian(3)
+                    .ThresholdBinaryInv(new Gray(240), new Gray(255));
+
+                // get contours of source and target images
+                var imgSourceContours = CalculateContours(imgSource);
+                var imgTargetContours = CalculateContours(imgTarget);
+
+                // exception handling
+                if (imgSourceContours.Size == 0 || imgTargetContours.Size == 0)
+                {
+                    throw new Exception("Not enough contours");
+                }
+
+                //loops through all contours in image and comparing to target
+                for (int i = 0; i < imgSourceContours.Size; i++)
+                {
+                    var distance = CvInvoke.MatchShapes(imgSourceContours[i], imgTargetContours[0], Emgu.CV.CvEnum.ContoursMatchType.I2);
+
+                    if (distance<=threshold)
+                    {
+                        var rect = CvInvoke.BoundingRectangle(imgSourceContours[i]);
+                        img.Draw(rect, new Bgr(0, 255, 0), 4);
+                        CvInvoke.PutText(img, distance.ToString("F6"), new Point(rect.X, rect.Y + 20),
+                            Emgu.CV.CvEnum.FontFace.HersheyPlain, 3, new MCvScalar(255, 0, 0));
+                    }
+                }
+
+                pictureBox1.Image = img.ToBitmap();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private VectorOfVectorOfPoint CalculateContours(Image<Gray, byte> img, double thresholdarea = 1000)
+        {
+            try
+            {
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat h = new Mat();
+
+                CvInvoke.FindContours(img, contours, h, Emgu.CV.CvEnum.RetrType.External,
+                    Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+                VectorOfVectorOfPoint filteredContours = new VectorOfVectorOfPoint();
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var area = CvInvoke.ContourArea(contours[i]);
+                    if (area >= thresholdarea)
+                    {
+                        filteredContours.Push(contours[i]);
+                    }
+                }
+
+                return filteredContours;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
